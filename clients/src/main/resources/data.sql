d9693fff79b4e86283f1e28b1019a8609bf0f45d
pari <ngpari.earth@gmail.com>
2024-01-16 11:40:15 +0530

    minor changes

--- clients/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-delete from customer;
-
-insert into customer(name) values ('name1');
-insert into customer(name) values ('name2');
-insert into customer(name) values ('name3');
-insert into customer(name) values ('name4');
-insert into customer(name) values ('name5');
